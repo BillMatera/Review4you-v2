b98c1730ee1c3e70074ec983ff77b901acef1068
BillMatera <matera.3@osu.edu>
2017-08-03 00:58:58 -0400

    map

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 from flask import Flask
 from flask import render_template
 from datetime import datetime
+from flask_googlemaps import GoogleMaps
 
 app = Flask(__name__)
+GoogleMaps(app, key="AIzaSyC-IaoIh43rhygYYBAfbhHJuuzbZWKkhII")
+
 @app.route('/')
 def index():
 	return render_template(
