b330fd665b472fc06daffdd30eaa9d04f12427e6
BillMatera <matera.3@osu.edu>
2017-08-03 00:39:09 -0400

    more

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 
 from flask import Flask
+from flask import render_template
+from datetime import datetime
+
 app = Flask(__name__)
 @app.route('/')
 def index():
-	return "Yo, it's working!"
+	return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+    
 if __name__ == "__main__":
 	app.run()
